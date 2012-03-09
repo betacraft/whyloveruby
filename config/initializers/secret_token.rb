da19ecf0a29e67d62ce4bf9123ba379175d9c433
rtdp <ratnadeepdeshmane@gmail.com>
2012-03-09 12:00:17 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myloveruby::Application.config.secret_token = '28bb257ca354da9fe60a54dabb01e8b5d31589ebb853acb10f97ad7148e6690d16f8b64d0ca872ca22224d2afc3966768ebdd68c3a062a883d3484805b014077'
