715f1183439f29458d060d859e6ab6455932791c
rtdp <ratnadeepdeshmane@gmail.com>
2012-03-11 21:23:47 +0530

    excpetion notification added.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,10 @@ Myloveruby::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  # exception notification - 
+  Whatever::Application.config.middleware.use ExceptionNotifier,
+      :email_prefix => "[My Love Ruby] ",
+      :sender_address => %{"notifier" <notifier@example.com>},
+      :exception_recipients => %w{rtdp@rainingclouds.com}
 end
