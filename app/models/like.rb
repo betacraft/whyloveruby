7346b49cf6c914fe8cef96c0a2189d3e444a23e0
rtdp <ratnadeepdeshmane@gmail.com>
2012-03-11 19:12:52 +0530

    bit corrections with like

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,7 +3,7 @@ class Like < ActiveRecord::Base
   belongs_to :user
   belongs_to :letter
 
-  #validates_uniqueness_of :user_id, scope: :letter_id
+  validates_uniqueness_of :user_id, scope: :letter_id
   validates_presence_of :user_id, :letter_id
 
 end
