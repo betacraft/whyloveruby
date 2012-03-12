8f5d867bc2a1bdfef2d76f97b1f6ed3d9ae79675
rtdp <ratnadeepdeshmane@gmail.com>
2012-03-12 20:07:08 +0530

    remaining files from admin added.

---
+++ app/admin/letter.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Letter do
+
+  index do 
+    column :id
+    column :description do |l|
+      l.description.truncate(200)
+    end
+    column :user_name do |l|
+      l.user.name
+    end
+    column :likes do |l|
+      l.likes.count
+    end
+    column :created_at
+    default_actions
+  end
+
+end
---
+++ app/admin/user.rb
@@ -0,0 +1,19 @@
+  ActiveAdmin.register User do
+
+    index do
+      column :id
+      column :twitter_handle
+      column :name
+      column :last_sign_in_at
+      column :created_at
+      column :sign_in_count
+      column :letters_count do |user|
+        user.letters.count
+      end
+      column :likes do |user|
+        user.likes.count
+      end
+      default_actions
+    end
+
+  end
