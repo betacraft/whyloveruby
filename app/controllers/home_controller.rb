e03797c0137eecc6e26c9f21627cc67d40c09999
rtdp <ratnadeepdeshmane@gmail.com>
2012-03-12 20:16:07 +0530

    fix for ranks added.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,8 @@ class HomeController < ApplicationController
 
   def index
     @letter = Letter.new
-    #@letters = Letter.paginate(page: params[:page], per_page: 15, order: '(SELECT COUNT(*) FROM likes WHERE (letters.id=likes.letter_id)) DESC', include: [:likes, :user])
-    @letters = Letter.page(params[:page]).per(1)
+    #@letters = Letter.paginate(page: params[:page], per_page: 15, 
+    @letters = Letter.order('(SELECT COUNT(*) FROM likes WHERE (letters.id=likes.letter_id)) DESC').includes([:likes, :user]).page(params[:page]).per(15)
     @recent_letters = Letter.limit(5).order('created_at DESC').includes(:user)
   end
 
