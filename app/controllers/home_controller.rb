86cb4dd58116952faaeb7636c0d4387bb7da32e5
rtdp <ratnadeepdeshmane@gmail.com>
2012-03-12 20:14:10 +0530

    kaminari integration done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,8 @@ class HomeController < ApplicationController
 
   def index
     @letter = Letter.new
-    @letters = Letter.paginate(page: params[:page], per_page: 15, order: '(SELECT COUNT(*) FROM likes WHERE (letters.id=likes.letter_id)) DESC', include: [:likes, :user])
+    #@letters = Letter.paginate(page: params[:page], per_page: 15, order: '(SELECT COUNT(*) FROM likes WHERE (letters.id=likes.letter_id)) DESC', include: [:likes, :user])
+    @letters = Letter.page(params[:page]).per(1)
     @recent_letters = Letter.limit(5).order('created_at DESC').includes(:user)
   end
 
