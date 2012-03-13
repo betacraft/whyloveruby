05246496d02507cab126ea4d88ed396bab09b9a3
rtdp <ratnadeepdeshmane@gmail.com>
2012-03-13 16:27:43 +0530

    recent limit increased

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
     @letter = Letter.new
     #@letters = Letter.paginate(page: params[:page], per_page: 15, 
     @letters = Letter.order('(SELECT COUNT(*) FROM likes WHERE (letters.id=likes.letter_id)) DESC').includes([:likes, :user]).page(params[:page]).per(15)
-    @recent_letters = Letter.limit(5).order('created_at DESC').includes(:user)
+    @recent_letters = Letter.limit(15).order('created_at DESC').includes(:user)
   end
 
 end
