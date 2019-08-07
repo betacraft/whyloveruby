3552b6e92f063a76afc20f9a42e4bfdab7eb4131
anubhav8421 <anubhavj1357@gmail.com>
2019-08-07 18:25:18 +0530

    #8 - Add configuration for sentry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_action :set_raven_context
+
+  private
+    def set_raven_context
+      Raven.user_context(id: session[:current_user_id])
+      Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+    end
 end
 
