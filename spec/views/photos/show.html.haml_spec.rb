d3444e12ad89587e5fe7681cba580b73e8326f6a
pradnyeshpatil <patilpradnyesh1312@gmail.com>
2021-02-05 15:24:27 +0530

    resolve rspec errors

--- spec/views/photos/show.html.haml_spec.rb
+++ spec/views/photos/show.html.haml_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "photos/show", type: :view do
-  image= File.open("/home/shyam/betacraft/whyloveruby/public/forkme_right_red.png")
+  image= File.open("public/forkme_right_red.png")
   before(:each) do
     @photo = assign(:photo, Photo.create!(
       image: image
