bc44e776f5f0709844ba5599d2480e93f0dc7d2b
Yug-gurnani <yug.gurnani091@gmail.com>
2021-09-08 16:14:53 +0530

    Removed require master key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,7 +118,4 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-
-  # As by default the production db will look for the secret key in secrets.yml
-  config.require_master_key = true
 end
