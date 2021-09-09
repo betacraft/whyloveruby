50cd0dc7658290c5b49447749275eea335c45cfe
Yug-gurnani <yug.gurnani091@gmail.com>
2021-09-09 14:27:34 +0530

    Added a helper function for getting user image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
-
-
+  def get_twitter_image(twitter_handle)
+    "https://res.cloudinary.com/#{ENV['CLOUDINARY_HANDLE']}/image/twitter_name/#{twitter_handle}.jpg"
+  end
 end
