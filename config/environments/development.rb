e5507a8ae86d332b593c5fc531f4506406e7f053
Aniket Kaushik <aniketkaushik@outlook.in>
2023-12-28 17:24:25 +0530

    chore: added letter opener to preview email in the browser

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,6 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 end
